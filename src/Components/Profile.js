f522b8ccb3a52641fb445c83ee43b5b392c8e1b2
Casey Harding <casey.r.harding@gmail.com>
2022-05-04 14:04:18 -0400

    hotfix for removing comments

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,17 +1,3 @@
-/**
- * 
- const initialUser = {
-  firstName: "Casey",
-  lastName: "Harding",
-  username: "CRHarding",
-  profilePic: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.amcnetworks.com%2Fbbcamerica.com%2Fwp-content%2Fuploads%2F2017%2F04%2Fanglo_2000x1125_thorgladiator.jpg&f=1&nofb=1",
-  email: "c@c.com",
-  age: 72,
-  about: "Lorem ipsum dolor sit amet.",
-  posts: []
-}
- */
-
 import React from "react";
 
 const Profile = (props) => {
